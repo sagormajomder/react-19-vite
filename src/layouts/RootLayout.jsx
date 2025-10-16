d52b3a9c81ee0befe6c9e0e02ee30e275e6f8ec5
Sagor Majomder <sagormajomder@gmail.com>
2025-10-16 14:46:56 +0600

    Add Container component and refactor layout structure

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,13 +1,20 @@
 import { Outlet } from "react-router";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
+
+import Footer from "./../components/Footer";
+import Header from "./../components/Header";
 
 export default function RootLayout() {
   return (
     <div className="grid min-h-dvh grid-rows-[auto_1fr_auto]">
       <Header />
-      <Outlet />
+      <Main>
+        <Outlet />
+      </Main>
       <Footer />
     </div>
   );
+}
+
+function Main({ children }) {
+  return <main>{children}</main>;
 }
